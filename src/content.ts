6d5268c6e3e20b25119ec345f92d8d13495890ff
Moritz Salla <moritzsalla@moritz-mbp.local>
2020-09-18 12:50:31 +0200

    minor

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,18 @@
 console.log("browser extension is active");
 
-const headings: any = document.getElementsByTagName("h1");
-const subheadings: any = document.getElementsByTagName("h2");
+chrome.runtime.sendMessage({ myQuestion: "Is it ON or OFF?" }, function (
+  response
+) {
+  console.log("Extension state is: " + response.state); // should be ON
+  if (response.state !== "ON") return;
 
-document.querySelector("body").style.background = "red";
-document.querySelector("html").style.background = "red";
+  const headings: any = document.getElementsByTagName("h1");
+  const subheadings: any = document.getElementsByTagName("h2");
 
-document.querySelectorAll("div").forEach((elem) => {
-  elem.style.background = "red";
+  document.querySelector("body").style.background = "red";
+  document.querySelector("html").style.background = "red";
+
+  document.querySelectorAll("div").forEach((elem) => {
+    elem.style.background = "red";
+  });
 });
