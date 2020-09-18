88be485e3537da94bef5d097d491ed8b86617471
Moritz Salla <moritzsalla@moritz-mbp.local>
2020-09-18 11:30:30 +0200

    resolved ts issues

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,15 @@
-console.log("content heya");
+console.log("browser extension is active");
+
+const headings: HTMLCollectionOf<
+  HTMLHeadingElement
+> | null = document.getElementsByTagName("h1");
+
+const subheadings = document.getElementsByTagName("h2");
+
+for (let item of headings) {
+  console.log(item);
+}
+
+for (let item of subheadings) {
+  console.log(item);
+}
