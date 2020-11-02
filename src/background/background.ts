cd7317d0965d4068354277a3afe2844caadbd7cf
moritzsalla <moritz.salla@hotmail.de>
2020-11-02 13:48:00 +0000

    added linear interpolation and normalised values

--- src/background/background.ts
+++ src/background/background.ts
@@ -21,21 +21,30 @@ chrome.runtime.onInstalled.addListener(function () {
 const Sense = new SenseHat("192.168.0.24", true);
 
 // init animation loop and do smth with sensehat
+
+let x = 1;
+let easing = 0.1;
+
 setInterval(function () {
   chrome.storage.local.get(["score"], (result) => {
     if (result.score.constructor === Array) {
-      const x = result.score[result.score.length - 1];
+      let val = result.score[result.score.length - 1];
 
-      const color = {
-        r: Math.round(x * 255),
-        g: 0,
-        b: Math.round(x * 255),
-      };
-      const { r, g, b } = color;
+      // ease
+      let dist = val - x;
+      x += dist * easing;
+
+      // map to rgb range
+      let normX = Math.round(map(x, 0, 1, 0, 255));
+
+      console.log({
+        val: val,
+        x: x,
+        normalizedX: normX,
+      });
 
       try {
-        Sense.setColor(r, g, b);
-        console.log(`Sending ${r} ${g} ${b} to sensehat.`);
+        Sense.setColor(normX, 0, normX);
       } catch (err) {
         console.error(err);
         console.error(
@@ -45,3 +54,13 @@ setInterval(function () {
     }
   });
 }, 400);
+
+function map(
+  value: number,
+  start1: number,
+  stop1: number,
+  start2: number,
+  stop2: number
+): number {
+  return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
